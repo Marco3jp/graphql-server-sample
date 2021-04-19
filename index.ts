ba69ad7e26a4027f4e138fa30eeb98cf5139ceaf
Marco <marco3jp+git@gmail.com>
2021-04-19 19:27:47 +0900

    impl posting review

--- index.ts
+++ index.ts
@@ -1,16 +1,41 @@
 import {ApolloServer, gql} from 'apollo-server';
+import { v4 as uuidv4 } from 'uuid';
 import * as fs from "fs";
 
 const typeDefs = gql`
+    type User {
+        id: ID!
+        name: String!
+        reviews: [Review!]
+    }
+    
+    input ReviewInput {
+        userId: ID!
+        storeId: ID!
+        reviewText: String!
+    }
+
+    type Review {
+        id: ID!
+        userId: ID!
+        storeId: ID!
+        reviewText: String!
+    }
 
     type Store {
         id: ID!
         name: String!
         address: String
+        reviews: [Review!]
     }
 
     type Query {
         stores: [Store!]!
+        users: [User!]!
+    }
+    
+    type Mutation {
+        postReview(review: ReviewInput): Review!
     }
 `;
 
@@ -18,8 +43,25 @@ const database = loadDatabase();
 
 const resolvers = {
     Query: {
-        stores: () => database.stores
+        stores: () => database.stores,
+        users: () => database.users
     },
+    Mutation: {
+        postReview: (_, {review}) => {
+            review.id = uuidv4();
+            const targetStore = database.stores.find(store => store.id === review.storeId);
+            const postingUser = database.users.find(user => user.id === review.userId);
+
+            targetStore?.reviews ? targetStore.reviews.push(review) : targetStore.reviews = [review];
+            postingUser?.reviews ? postingUser.reviews.push(review) : postingUser.reviews = [review];
+
+            console.log(targetStore);
+            console.log(database.stores);
+
+            saveDatabase(database);
+            return review;
+        }
+    }
 };
 
 const server = new ApolloServer({typeDefs, resolvers});
@@ -27,7 +69,6 @@ const server = new ApolloServer({typeDefs, resolvers});
 server.listen().then(({url}) => {
     console.log(`🚀  Server ready at ${url}`);
 });
-
 
 function loadDatabase() {
     const file = fs.readFileSync('database.json', {encoding: "utf8"});
