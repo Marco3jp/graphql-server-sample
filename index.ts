26798d96a8ae9fffdd3c7024b4d3c7df806f50c4
Marco <marco3jp+git@gmail.com>
2021-04-20 17:00:05 +0900

    change review props from external id to external data

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import {ApolloServer, gql} from 'apollo-server';
-import { v4 as uuidv4 } from 'uuid';
+import {v4 as uuidv4} from 'uuid';
 import * as fs from "fs";
 
 const typeDefs = gql`
@@ -8,7 +8,7 @@ const typeDefs = gql`
         name: String!
         reviews: [Review!]
     }
-    
+
     input ReviewInput {
         userId: ID!
         storeId: ID!
@@ -17,8 +17,8 @@ const typeDefs = gql`
 
     type Review {
         id: ID!
-        userId: ID!
-        storeId: ID!
+        user: User!
+        store: Store!
         reviewText: String!
     }
 
@@ -33,7 +33,7 @@ const typeDefs = gql`
         stores: [Store!]!
         users: [User!]!
     }
-    
+
     type Mutation {
         postReview(review: ReviewInput): Review!
     }
@@ -47,16 +47,22 @@ const resolvers = {
         users: () => database.users
     },
     Mutation: {
-        postReview: (_, {review}) => {
-            review.id = uuidv4();
-            const targetStore = database.stores.find(store => store.id === review.storeId);
-            const postingUser = database.users.find(user => user.id === review.userId);
+        postReview: (_, {review: reviewInput}) => {
+            const targetStore = database.stores.find(store => store.id === reviewInput.storeId);
+            const postingUser = database.users.find(user => user.id === reviewInput.userId);
+
+            const review = {
+                id: uuidv4(),
+                reviewText: reviewInput.reviewText,
+                store: targetStore,
+                user: postingUser
+            };
 
             targetStore?.reviews ? targetStore.reviews.push(review) : targetStore.reviews = [review];
             postingUser?.reviews ? postingUser.reviews.push(review) : postingUser.reviews = [review];
 
             saveDatabase(database);
-            return review;
+            return reviewInput;
         }
     }
 };
